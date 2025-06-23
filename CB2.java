7a42b0e5eee6d6becc638b916f09088ef85ac7fa
Dhrumilmehta21 <88776434+Dhrumilmehta21@users.noreply.github.com>
2025-06-23 15:32:14 +0530

    Add files via upload

---
+++ CB2.java
@@ -0,0 +1,17 @@
+import javax.swing.*;   
+public class CB2 {   
+ 
+CB2(){   
+  JFrame f=new JFrame();   
+    String s[]={"Maharashtra","Punjab","Gujrat","TamilNadu"};       
+    JComboBox cb=new JComboBox(s);   
+    cb.setBounds(50, 50,90,20);   
+    f.add(cb);       
+    f.setLayout(null);   
+    f.setSize(400,400);   
+    f.setVisible(true);       
+}   
+public static void main(String[] args) {   
+    new CB2();       
+}   
+}
